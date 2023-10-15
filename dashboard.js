576ae55f02b786d2bb346115efbe662b006ef4fd
Pistolk1 <97509308+Pistolk1@users.noreply.github.com>
2023-10-15 14:22:53 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -3,61 +3,26 @@ document.addEventListener("DOMContentLoaded", function() {
     var username = localStorage.getItem("username");
     document.getElementById("username").textContent = username;
 
-    // Retrieve profile picture URL from localStorage
-    var savedProfilePictureUrl = localStorage.getItem(username + "_profile_picture");
-    var profilePicture = document.getElementById("profile-picture");
+    // Fetch posts from the server-side API
+    fetch("/api/posts")
+        .then(response => response.json())
+        .then(posts => {
+            // Display posts from different users
+            posts.forEach(post => {
+                displayPost(post.username, post.content);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching posts:', error);
+        });
 
-    if (savedProfilePictureUrl) {
-        profilePicture.src = savedProfilePictureUrl;
-    }
-
-    // Profile picture upload functionality
-    var profilePictureInput = document.getElementById("profile-picture-input");
-    profilePictureInput.addEventListener("change", function(event) {
-        var file = event.target.files[0];
-        var reader = new FileReader();
-
-        reader.onloadend = function() {
-            profilePicture.src = reader.result;
-            // Save profile picture URL to localStorage
-            localStorage.setItem(username + "_profile_picture", reader.result);
-        };
-
-        if (file) {
-            reader.readAsDataURL(file);
-        }
-    });
-
-    // Retrieve and display saved posts from localStorage
-    var savedPosts = JSON.parse(localStorage.getItem(username + "_posts")) || [];
-
-    savedPosts.forEach(function(postContent) {
-        displayPost(postContent);
-    });
-
-    // Post functionality
-    var postButton = document.getElementById("post-button");
-    var postText = document.getElementById("post-text");
-
-    postButton.addEventListener("click", function() {
-        var postContent = postText.value;
-        if (postContent.trim() !== "") {
-            // Save the post content to localStorage
-            savedPosts.push(postContent);
-            localStorage.setItem(username + "_posts", JSON.stringify(savedPosts));
-            // Display the post
-            displayPost(postContent);
-            // Clear the textarea after posting
-            postText.value = "";
-        } else {
-            alert("Please enter your post content.");
-        }
-    });
-
-    function displayPost(postContent) {
+    // Profile picture upload and post functionality (similar to previous examples)
+    // ...
+    
+    function displayPost(author, postContent) {
         var postContainer = document.createElement("div");
         postContainer.className = "post";
-        postContainer.textContent = postContent;
+        postContainer.innerHTML = `<strong>${author}:</strong> ${postContent}`;
         document.body.appendChild(postContainer);
     }
 });
